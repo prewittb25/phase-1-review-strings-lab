e79743f278de2834fab501ddf4c54a3e9047a397
Barry <Prewittb25@gmail.com>
2023-01-10 08:57:17 -0500

    almost complete

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+
+const currentUser = "Barry"
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase(currentUser)
+
+const shortGreeting =  `Welcome, ${currentUser}!` 
+
+ const firstInitial = currentUser.charAt(1)
+
